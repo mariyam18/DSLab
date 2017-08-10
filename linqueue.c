a94a12c91a0b8a0d078aa1668ee6e5e59ca2b471
mariyam18 <mariyam.anishaikh18@gmail.com>
2017-08-10 11:48:36 +0530

    linqueue.c

---
+++ linqueue.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 10
+
+int queue[MAX_SIZE],front=-1,rear=-1;
+
+int isFull(){
+	//returns 1 if queue is full else returns -1
+	return rear==MAX_SIZE-1;
+	
+}
+
+int isEmpty(){
+//returns 1 if queue is empty else returns -1
+	return front==-1 && rear==-1;
+	
+}
+
+int peek(){
+//return element at the top of queue
+	return queue[front];
+}
+
+ void insertqueue(int d)
+{
+	
+	 if(!isFull())
+	 {
+		 if(isEmpty())
+		 {
+			 front++;
+		 }
+		 rear++;
+		 queue[rear]=d;
+		 printf("%d is sucessfully inserted",d);
+	}
+	else
+	printf("Queue is full element cannot be inserted\n");
+}	 	 
+
+void deletqueue()
+{
+	int d;
+	if(!isEmpty())
+	{
+		d=queue[front];
+		if(front==rear)
+		{
+			front=rear=-1;
+		}
+		else
+	    {
+			front++;
+		}
+		printf("Element deleted is %d",d);
+    }
+    else{
+		printf("queue is empty");
+	}
+}
+	 
+
+
+
+int main(){
+	int choice,e;
+	do
+	{
+		//display menu 1.peek 2.insert 3.delet 4.exit
+		printf("\t\t\t\nMENU\n1.peek\n2.Insert\n3.Delet\n4.Exit\n");
+		printf("Enter your choice: ");
+		scanf("%d",&choice);	//take input in choice variable
+		switch(choice)
+		{
+			case 1:
+				if(isEmpty())
+				printf("Queue is Empty");
+				else
+				{
+				e=peek();//call peek function
+				printf("\nElement at the front of queue is : %d\n",e);
+			    }
+			    
+				break;
+			case 2:
+				printf("\nEnter the elment to be inserted: \n");
+				scanf("%d",&e);
+				insertqueue(e);//call insert function
+				break;
+			case 3:
+				deletqueue();//call delet function
+				break;
+			case 4:
+				exit(0);//call exit(0) function or return 0
+				break;
+			default:
+				printf("\nInvalid Choice");//invalid choice
+				break;				
+		}
+	}while(1);
+	return 0;
+}
--- stack.c
+++ stack.c
@@ -1,89 +1,82 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define MAX_SIZE 10
 
 int stack[MAX_SIZE],top=-1;
+
 int isFull(){
-//returns 1 if stack is full else returns -1
- return top==MAX_SIZE-1;
- 
+	//returns 1 if stack is full else returns -1
+	return top==MAX_SIZE-1;
+	
 }
 
 int isEmpty(){
 //returns 1 if stack is empty else returns -1
-return top==-1;
-
+	return top==-1;
+	
 }
 
 int peek(){
 //return element at the top of stack
- return stack[top];
+	return stack[top];
 }
 
 void push(int e){
 //inserts an element into stack
-if (!isFull())
-{
-	top++;
-	int e;
-	stack[top]=e;
-	printf(" element %d is successfully inserted",e);
-}
-else{
-	printf("stack overflow\n");
-	
-}
+	if(!isFull())
+	{
+		top++;
+		stack[top]=e;
+		printf("\nYour element %d is successfully inserted",e);
+	}
+	else{
+		
+		printf("\nElement cannot be inserted. Stack Overflow!");
+	}
 }
 
 void pop(){
-//delets element from stack	
-int d;
-if(!isEmpty())	
-{
-	d= stack[top];
-	top--;
-	printf("msg element %d successfully deleted",d);
-}
-else{
-	printf("msg stack underflow");
-}			
+//deletes an element from top of stack
+	int d;
+	if(!isEmpty())
+	{
+		d=stack[top];
+		top--;
+		printf("\nYour element %d is successfully deleted",d);
+	}
+	else{
+		printf("\nElement cannot be deleted. Stack Underflow!");
+	}
 }
 
-int main()
-{
+int main(){
 	int choice,e;
 	do
 	{
-		printf ("\t\t\tMENU \n1.peek \n2.push \n3.pop \n4.exit\n");
-         printf("\n Enter  your choice\n");
-         scanf("%d",&choice);
-		//take input in choice variable
-		switch(choice)
-		{
+		//display menu 1.peek 2.push 3.pop 4.exit
+		printf("\t\t\nMENU\n1.Peek\n2.Push\n3.Pop\n4.Exit\n");
+		printf("Enter your choice: ");
+		scanf("%d",&choice);	//take input in choice variable
+		switch(choice){
 			case 1:
-			e=peek(stack);
-			printf("%d",e);    //call peek function
-			break;
-			
+				e=peek();//call peek function
+				printf("\nElement at the top of stack is : %d",e);
+				break;
 			case 2:
-			printf("\n enter the element\n");
-			scanf("%d",&e);
-			push(e);   //call push function
-			break;
-			
-		    case 3:
-		    pop(stack);  //call pop function
-			break;
-			
-		    case 4:
-		    return(0);   //call exit(0) function
-			break;
-			
+				printf("\nEnter the elment to be inserted: ");
+				scanf("%d",&e);
+				push(e);//call push function
+				break;
+			case 3:
+				pop();//call pop function
+				break;
+			case 4:
+				exit(0);// return 0
+				break;
 			default:
-			printf("\n invalid choice\n");
-			break;
-	}
-  }while(1);
-   return 0;
-}				
-		
-		
+				printf("\nInvalid Choice");//invalid choice
+				break;				
+		}
+	}while(1);
+	return 0;
+}
