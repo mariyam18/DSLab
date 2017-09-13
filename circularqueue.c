1018e9c1206ed06c7f23e9c88984aa16cfb84c42
mariyam18 <mariyam.anishaikh18@gmail.com>
2017-09-13 11:46:43 +0530

    circularqueue

--- circularqueue.c
+++ circularqueue.c
@@ -1,103 +1,232 @@
+/*Description: Implementation of circular queue using array
+  Learner: Shaikh Mariyam Anis.
+  */
+ 
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>  //for absolute function
 #define MAX_SIZE 10
 
 int queue[MAX_SIZE],front=-1,rear=-1;
 
 int isFull(){
-	//returns 1 if queue is full else returns -1
-	return ((abs(front-rear)%(MAX_SIZE-1)==1 || abs(front-rear)%(MAX_SIZE-1)==0)&& (front!=-1&&rear!=-1)) ;
-	
+	return ((rear==MAX_SIZE-1 && front==0)||(rear+1==front));	
 }
 
 int isEmpty(){
-//returns 1 if queue is empty else returns -1
-	return front==-1 && rear==-1;
-	
+	return front==-1&&rear==-1;	
 }
 
-int peek(){
-//return element at the top of queue
-	return queue[front];
+void display(){
+	int i=front;
+	if(!isEmpty()){
+		while(i!=rear){
+			printf("%d\t",queue[i]);
+			i=(i+1)%MAX_SIZE;
+		}
+		printf("%d\t",queue[i]);
+	}
+	else{
+		printf("\nQueue is Empty.");
+	}	
+			
 }
 
- void insertqueue(int d)
-{
-	
-	 if(!isFull())
-	 {
-		 if(isEmpty())
-		 {
-			 front++;
-		 }
-		 rear=(rear++)%MAX_SIZE;
-		 queue[rear]=d;
-		 printf("%d is sucessfully inserted",d);
+void insertqueue(int e){
+	if(!isFull()){
+		rear=(rear+1)%(MAX_SIZE);
+		queue[rear]=e;
+		if(front==-1)
+			front++;
+		printf("\nElement %d is successfully Inserted",e);
+		printf("\n%d-%d",front,rear);
 	}
 	else
-	printf("Queue is full element cannot be inserted\n");
-}	 	 
+		printf("\nQueue is Full Element cannot be inserted.");	
+}
 
-void deletqueue()
-{
+void deletequeue(){
 	int d;
-	if(!isEmpty())
-	{
+	if(!isEmpty()){
 		d=queue[front];
 		if(front==rear)
-		{
 			front=rear=-1;
-		}
 		else
-	    {
-			front=(front++)%MAX_SIZE;
-		}
-		printf("Element deleted is %d",d);
-    }
-    else{
-		printf("queue is empty");
+			front=(front+1)%(MAX_SIZE);
+		printf("\nThe element deleted is %d",d);
+		printf("\n%d-%d",front,rear);	
 	}
+	else
+		printf("\nQueue is Empty");	
 }
-	 
-
-
 
 int main(){
 	int choice,e;
 	do
 	{
-		//display menu 1.peek 2.insert 3.delet 4.exit
-		printf("\t\t\t\nMENU\n1.peek\n2.Insert\n3.Delet\n4.Exit\n");
+		printf("\n\nEnter\n1.Display\n2.Insert\n3.Delete\n4.Exit\n");
 		printf("Enter your choice: ");
 		scanf("%d",&choice);	//take input in choice variable
-		switch(choice)
-		{
+		switch(choice){
 			case 1:
-				if(isEmpty())
-				printf("Queue is Empty");
-				else
-				{
-				e=peek();//call peek function
-				printf("\nElement at the front of queue is : %d\n",e);
-			    }
-			    
+				printf("\nElements of queue are ");
+				display();
 				break;
 			case 2:
-				printf("\nEnter the elment to be inserted: \n");
+				printf("\nEnter the elment to be inserted: ");
 				scanf("%d",&e);
-				insertqueue(e);//call insert function
+				insertqueue(e);
 				break;
 			case 3:
-				deletqueue();//call delet function
+				deletequeue();
 				break;
 			case 4:
 				exit(0);//call exit(0) function or return 0
 				break;
 			default:
-				printf("\nInvalid Choice");//invalid choice
+				printf("\nInvalid Choice");
 				break;				
 		}
 	}while(1);
 	return 0;
 }
+/*Output
+  
+ 
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 2
+
+Enter the elment to be inserted: 10
+
+Element 10 is successfully Inserted
+0-0
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 2
+
+Enter the elment to be inserted: 20
+
+Element 20 is successfully Inserted
+0-1
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 2
+
+Enter the elment to be inserted: 30
+
+Element 30 is successfully Inserted
+0-2
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 2
+
+Enter the elment to be inserted: 40
+
+Element 40 is successfully Inserted
+0-3
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 2
+
+Enter the elment to be inserted: 50
+
+Element 50 is successfully Inserted
+0-4
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 1
+
+Elements of queue are 10	20	30	40	50	
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 3
+
+The element deleted is 10
+1-4
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 1
+
+Elements of queue are 20	30	40	50	
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 3
+
+The element deleted is 20
+2-4
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 1
+
+Elements of queue are 30	40	50	
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 3
+
+The element deleted is 30
+3-4
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 1
+
+Elements of queue are 40	50	
+
+Enter
+1.Display
+2.Insert
+3.Delete
+4.Exit
+Enter your choice: 4
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+*/
